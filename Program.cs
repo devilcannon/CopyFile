21eab633be39eafb20b9336ca5be0e42feaee8c1
JESUS NAVARRO <JUANJESUS_NAVARRO@Jabil.com>
2023-03-09 11:45:20 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace CopyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3 arguments: From, To, Type
+            if (checkExistPath(args[0], args[1]))
+            {
+                try
+                {
+                    Console.WriteLine("[START AT "+DateTime.Now.ToString()+"]");
+                    Console.WriteLine("Total files copied: "+CopyFilesRecursively(args[0], args[1], args[2]));
+                    Console.WriteLine("[FINISH AT " + DateTime.Now.ToString() + "]");
+                }
+                catch(Exception es)
+                {
+                    Console.WriteLine("Error getting files: "+es.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid paths");
+            }
+            Console.WriteLine("Finish, be happy :)");
+        }
+
+        private static bool checkExistPath(string from,string to)
+        {
+            bool isValid = false;
+            if (Directory.Exists(from) && Directory.Exists(to))
+                isValid = true;
+            return isValid;
+        }
+
+        private static int CopyFilesRecursively(string sourcePath, string targetPath,string type)
+        {
+            int totalFiles = 0;
+            //Now Create all of the directories
+            //foreach (string dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
+            //{
+            //    Directory.CreateDirectory(dirPath.Replace(sourcePath, targetPath));
+            //}
+
+            //Copy all the files & Replaces any files with the same name
+            foreach (string newPath in Directory.GetFiles(sourcePath, "*."+type, SearchOption.AllDirectories))
+            {
+                //File.Copy(newPath, newPath.Replace(sourcePath, targetPath), true);//Copy subfolders too
+                File.Copy(newPath,targetPath+"\\"+Path.GetFileName(newPath), true);
+                totalFiles++;
+            }
+            return totalFiles;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,17 @@
+﻿# Easy file mover
+Allow to move files filtering by type from point A to B
+
+## Technologies
+Console app made in C# and .NET Framework 4.5
+
+## License
+Feel free to use, I made it for fun
+
+## How to use it?
+1. Compile the project
+2. Over .exe output path run with the next syntax: CopyFile.exe [from] [to] [type]
+3. Example: C:\\Test\\CopyFile.exe C:\\Test\\folder1\\ C:\\Test\\folder2\\ txt
+
+## To do
+- Save the last index search
+- Remove commented code
