4e8dea901c688c5a85a879fbb30879057df0b7a5
JESUS NAVARRO <JUANJESUS_NAVARRO@Jabil.com>
2023-03-24 15:44:33 -0600

    Move only today files

--- Program.cs
+++ Program.cs
@@ -25,7 +25,6 @@ namespace CopyFile
             {
                 Console.WriteLine("Invalid paths");
             }
-            Console.WriteLine("Finish, be happy :)");
         }
 
         private static bool checkExistPath(string from,string to)
@@ -39,18 +38,15 @@ namespace CopyFile
         private static int CopyFilesRecursively(string sourcePath, string targetPath,string type)
         {
             int totalFiles = 0;
-            //Now Create all of the directories
-            //foreach (string dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
-            //{
-            //    Directory.CreateDirectory(dirPath.Replace(sourcePath, targetPath));
-            //}
-
             //Copy all the files & Replaces any files with the same name
             foreach (string newPath in Directory.GetFiles(sourcePath, "*."+type, SearchOption.AllDirectories))
             {
-                //File.Copy(newPath, newPath.Replace(sourcePath, targetPath), true);//Copy subfolders too
-                File.Copy(newPath,targetPath+"\\"+Path.GetFileName(newPath), true);
-                totalFiles++;
+                FileInfo info = new FileInfo(newPath);
+                if (DateTime.Today.DayOfYear == info.CreationTime.DayOfYear)
+                {
+                    File.Copy(newPath, targetPath + "\\" + Path.GetFileName(newPath), true);
+                    Console.WriteLine(++totalFiles);
+                }
             }
             return totalFiles;
         }
